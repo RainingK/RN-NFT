f2d5eb3fd0c5a6187667903264b7f887334e7f96
Ammar <ammar_98@live.com>
2023-05-25 15:07:09 +0400

    Added background

--- components/index.js
+++ components/index.js
@@ -1,5 +1,5 @@
 import HomeHeader from "./HomeHeader";
-import FocusedStatusBar from "./focusedStatusbar";
+import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
 
 export { HomeHeader, FocusedStatusBar, NFTCard };
