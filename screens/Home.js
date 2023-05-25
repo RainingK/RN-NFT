a2af537d3ffa32b12bdba0c87d223e863814a0c8
Ammar <37976857+RainingK@users.noreply.github.com>
2023-05-25 15:29:30 +0400

    Create Layout (#12)
    * Added Constants To App
    
    * Added basic files
    
    * Added status bar
    
    * Added background
    
    * Added Heart Button

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,29 @@
-import { Text } from 'react-native';
+import { FlatList, SafeAreaView, StatusBar, Text, View } from 'react-native';
+import { COLORS, NFTData } from '../constants';
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components';
 
 export const Home = () => {
 	return (
-		<Text>Home</Text>
+		<SafeAreaView style={ { flex: 1 } }>
+			<FocusedStatusBar background={ COLORS.primary } />
+
+			<View style={ { flex: 1 } }>
+				{/* Shows list of nft's */ }
+				<View style={ { zIndex: 0 } }>
+					<FlatList
+						data={ NFTData }
+						renderItem={ ( { item } ) => <NFTCard data={ item } /> }
+						keyExtractor={ ( item ) => item.id }
+						showsVerticalScrollIndicator={ false }
+						ListHeaderComponent={ <HomeHeader /> }
+					/>
+				</View>
+
+				<View style={ { position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, zIndex: -1 } }>
+					<View style={ { height: 300, backgroundColor: COLORS.primary } } />
+					<View style={ { flex: 1, backgroundColor: COLORS.white, height: 300 } } />
+				</View>
+			</View>
+		</SafeAreaView>
 	)
 }
