faf2a5dc90e2c1889869c32add47a5b863a20eca
Ammar <37976857+RainingK@users.noreply.github.com>
2023-05-26 15:31:51 +0400

    NFT-Details (#15)
    * Created Files
    
    * Added Top NFT Data
    
    * Added NFT Details
    
    * Added Current Bid Text
    
    * Finished The Details Bid Page

--- components/index.js
+++ components/index.js
@@ -1,6 +1,9 @@
-import HomeHeader from "./HomeHeader";
-import FocusedStatusBar from "./FocusedStatusBar";
-import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
+import { DetailsBid } from "./DetailsBid";
+import { DetailsDescription } from "./DetailsDescription";
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { SubInfo } from "./SubInfo";
 
-export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton };
+export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton, DetailsBid, DetailsDescription, SubInfo };
--- screens/Details.js
+++ screens/Details.js
@@ -1,7 +1,87 @@
-import { Text } from 'react-native';
+import { FlatList, Image, SafeAreaView, StatusBar, Text, View } from 'react-native';
+import { COLORS, FONTS, SHADOWS, SIZES, assets } from '../constants';
+import { CircleButton, DetailsBid, DetailsDescription, FocusedStatusBar, RectButton, SubInfo } from '../components';
+import React from 'react';
 
-export const Details = () => {
+const DetailsHeader = ( { data, navigation } ) => {
 	return (
-		<Text>Details</Text>
+		<View style={ { width: '100%', height: 373 } }>
+			<Image
+				source={ data.image }
+				resizeMode='cover'
+				style={ { width: '100%', height: '100%' } }
+			/>
+
+			<CircleButton
+				imgUrl={ assets.left }
+				handlePress={ () => navigation.goBack() }
+				left={ 15 }
+				top={ StatusBar.currentHeight + 10 }
+			/>
+			<CircleButton
+				imgUrl={ assets.heart }
+				right={ 15 }
+				top={ StatusBar.currentHeight + 10 }
+			/>
+		</View>
+	)
+}
+
+export const Details = ( { route, navigation } ) => {
+	const { data } = route.params;
+
+	return (
+		<SafeAreaView style={ { flex: 1 } }>
+			<FocusedStatusBar
+				barStyle="dark-content"
+				backgroundColor="transparent"
+				translucent={ true }
+			/>
+
+			<View style={ {
+				width: '100%',
+				position: 'absolute',
+				bottom: 0,
+				paddingVertical: SIZES.font,
+				justifyContent: 'center',
+				alignItems: 'center',
+				backgroundColor: 'rgba(255,255,255,0.5)',
+				zIndex: 1,
+			} }>
+				<RectButton
+					minWidth={ 170 }
+					fontSize={ SIZES.large }
+					{ ...SHADOWS.dark }
+				/>
+			</View>
+
+			<FlatList
+				data={ data.bids }
+				renderItem={ ( { item } ) => <DetailsBid bid={ item } /> }
+				keyExtractor={ ( item ) => item.id }
+				showsVerticalScrollIndicator={ false }
+				contentContainerStyle={ { paddingBottom: SIZES.extraLarge * 3 } }
+				ListHeaderComponent={ () => (
+					<React.Fragment>
+						<DetailsHeader data={ data } navigation={ navigation } />
+						<SubInfo />
+
+						<View style={ { padding: SIZES.font } }>
+							<DetailsDescription data={ data } />
+
+							{ data.bids.length > 0 && (
+								<Text style={ {
+									fontSize: SIZES.font,
+									fontFamily: FONTS.semiBold,
+									color: COLORS.primary
+								} }>
+									Current Bids
+								</Text>
+							) }
+						</View>
+					</React.Fragment>
+				) }
+			/>
+		</SafeAreaView>
 	)
 }
